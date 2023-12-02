016029df45970ca2a5b1f08b465b406eac241b4b
jakehase <jacobhasenkamp@gmail.com>
2023-12-01 23:25:56 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,26 @@
-//does the browser support service workers?
-
-if ("serviceWorker" in navigator) {
-  // defer service worker installation until page completes loading
-  window.addEventListener("load", () => {
-    //then register our service worker
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((reg) => {
-        //display a success message
-        console.log(`Service Worker Registration (Scope: ${reg.scope})`);
-      })
-      .catch((error) => {
-        //display an error message
-        console.log(`Service Worker Error (${error})`);
-      });
-  });
-} else {
-  //happens when the app isn't served over a TLS connection (HTTPS)
-  // or if the browser doesn't support the service worker
-  console.log("Service Worker not available");
+function registerServiceWorker() {
+    return navigator.serviceWorker.register('/sw.js');
 }
+
+function onServiceWorkerRegistered(registration) {
+    console.log(`SW Registered (Scope: ${registration.scope})`);
+}
+
+function onServiceWorkerRegistrationFailed(error) {
+    console.log(`SW Registration Failed: ${error}`);
+}
+
+function isServiceWorkerSupported() {
+    return "serviceWorker" in navigator;
+}
+
+if (isServiceWorkerSupported()) {
+    window.addEventListener("load", () => {
+        registerServiceWorker()
+            .then(onServiceWorkerRegistered)
+            .catch(onServiceWorkerRegistrationFailed);
+    });
+} else {
+    console.log("Browser does not support Service Workers");
+}
+
